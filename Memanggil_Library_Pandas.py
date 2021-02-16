685ebfa9cdec66a0812130592c245ed1c0b966c6
Ryan <ryanrizkyf@gmail.com>
2021-02-16 09:10:12 +0700

    initialization commit

---
+++ Memanggil_Library_Pandas.py
@@ -0,0 +1,8 @@
+# Pandas adalah library python open source yang biasanya digunakan untuk kebutuhan data analisis. 
+# Pandas membuat Python supaya dapat bekerja dengan data yang berbentuk tabular seperti spreadsheet dengan cara pemuatan data yang cepat, 
+# manipulasi data, menggabungkan data, serta ada berbagai fungsi yang lain.
+
+import pandas as pd
+import numpy as np
+
+# biasanya ketika menggunakan library Pandas, library Numpy juga diimport
