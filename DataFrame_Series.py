7f291f1cf5560ec51bd143b0071fc2f2e2d92878
Ryan <ryanrizkyf@gmail.com>
2021-02-16 09:15:18 +0700

    edit commit

---
+++ DataFrame_Series.py
@@ -0,0 +1,19 @@
+# Di Pandas terdapat 2 kelas data baru yang digunakan sebagai struktur dari spreadsheet:
+# 1. Series: satu kolom bagian dari tabel dataframe yang merupakan 1 dimensional numpy array sebagai basis data nya,
+# terdiri dari 1 tipe data (integer, string, float, dll).
+# 2. DataFrame: gabungan dari Series, berbentuk rectangular data yang merupakan tabel spreadsheet itu sendiri
+# (karena dibentuk dari banyak Series, tiap Series biasanya punya 1 tipe data, yang artinya 1 dataframe bisa memiliki banyak tipe data).
+
+import pandas as pd
+# Series
+number_list = pd.Series([1, 2, 3, 4, 5, 6])
+print("Series:")
+print(number_list)
+# DataFrame
+matrix = [[1, 2, 3],
+          ['a', 'b', 'c'],
+          [3, 4, 5],
+          ['d', 4, 6]]
+matrix_list = pd.DataFrame(matrix)
+print("DataFrame:")
+print(matrix_list)
